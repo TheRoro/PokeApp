8244b022736e96f596aa7dd75772091f3d3f248a
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-11-03 19:09:14 -0500

    Modified user input to lower case

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -9,11 +9,10 @@ class NameForm extends React.Component {
     }
   
     handleChange(event) {
-      this.setState({value: event.target.value});
+      this.setState({value: event.target.value.toLowerCase()});
     }
   
     handleSubmit(event) {
-      //('A name was submitted: ' + this.state.value);
       event.preventDefault();
       this.handleTypeChange();
     }
