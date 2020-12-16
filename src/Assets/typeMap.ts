eed91fd3f6e2525cf0ec63699cec2e34eb7eeffa
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-15 22:30:41 -0500

    Added useful assets
    Added pokemon chart matrix, dictionary and list for managing type calculations

---
+++ src/Assets/typeMap.ts
@@ -0,0 +1,22 @@
+let TypeMap = new Map([
+    ["bug", 0],
+    ["dark", 1],
+    ["dragon", 2],
+    ["electric", 3],
+    ["fairy", 4],
+    ["fighting", 5],
+    ["fire", 6],
+    ["flying", 7],
+    ["ghost", 8],
+    ["grass", 9],
+    ["ground", 10],
+    ["ice", 11],
+    ["normal", 12],
+    ["poison", 13],
+    ["pyschic", 14],
+    ["rock", 15],
+    ["steel", 16],
+    ["water", 17],
+]);
+
+export default TypeMap;
