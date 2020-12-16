ac488479e995b32461daab47f0f1e49865abe8ce
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-12-15 22:30:41 -0500

    Added useful assets
    Added pokemon chart matrix, dictionary and list for managing type calculations

---
+++ src/Assets/typeList.ts
@@ -0,0 +1,3 @@
+const TypeList = ['BUG', 'DARK', "DRAGON", "ELECTRIC", "FAIRY", "FIGHTING", "FIRE", "FLYING", "GHOST", "GRASS", "GROUND", "ICE", "NORMAL", "POISON", 'PSYCHIC', "ROCK", "STEEL", "WATER"];
+
+export default TypeList;
