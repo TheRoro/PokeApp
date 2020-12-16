1f2e76c2bb316d8b61304ee17a910ebd48d2987a
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-16 00:57:19 -0500

    Added Tuples Calculator
    Added Tuples for typeName and value.
    E,g: ["Bug", 0.5]

--- src/Assets/typeList.ts
+++ src/Assets/typeList.ts
@@ -1,3 +1,3 @@
-const TypeList = ['BUG', 'DARK', "DRAGON", "ELECTRIC", "FAIRY", "FIGHTING", "FIRE", "FLYING", "GHOST", "GRASS", "GROUND", "ICE", "NORMAL", "POISON", 'PSYCHIC', "ROCK", "STEEL", "WATER"];
+const TypeList = ["Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "GHOST", "GRASS", "GROUND", "ICE", "NORMAL", "POISON", 'PSYCHIC', "ROCK", "STEEL", "WATER"];
 
 export default TypeList;
--- src/components/Calculator2.tsx
+++ src/components/Calculator2.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import TypeChart from '../Assets/typeChart';
 import TypeMap from '../Assets/typeMap';
+import TypeList from '../Assets/typeList';
+
+const TOTAL_TYPES = 18;
 
 type CalculatorProps = {
     type1: string,
@@ -9,11 +12,13 @@ type CalculatorProps = {
 
 type IndexType = number | undefined;
 
+type ResultType = | undefined | [number, string][];
+
 const Calc: React.FC<CalculatorProps> = ({ type1, type2 }) =>{
     const [index1, setIndex1] = React.useState<IndexType>();
     const [index2, setIndex2] = React.useState<IndexType>();
-    const [result1, setResult1] = React.useState<Number[]>([]);
-    const [result2, setResult2] = React.useState<Number[]>([]);
+    const [mapResult, setMap] = React.useState([]);
+    const [finalResult, setFinalResult] = React.useState<ResultType>([]);
 
     const getIndex = (type: string) => {
         if (type) {
@@ -22,6 +27,59 @@ const Calc: React.FC<CalculatorProps> = ({ type1, type2 }) =>{
        return undefined;
     } 
 
+    const computeTypeCoverage = (type1: string, type2: string, index1: IndexType, index2: IndexType) => {
+        if(type1 === "" && type2 === ""){
+            alert("Pon un tipo ps wbn");
+            return undefined;
+        }
+        if(type1 === "" && typeof index2 === "number"){
+            let temp = [];
+            var pair: [number, string]
+            for (let i = 0; i < TOTAL_TYPES; i++) {
+                pair = [TypeChart[i][index2]*0.5, TypeList[i]];
+                temp.push(pair);
+            }
+            console.log(temp);
+            return temp;
+        }
+        if(type2 === "" && typeof index1 === "number"){
+            let temp = [];
+            var pair: [number, string]
+            for (let i = 0; i < TOTAL_TYPES; i++) {
+                pair = [TypeChart[i][index1]*0.5, TypeList[i]];
+                temp.push(pair);
+            }
+            console.log(temp);
+            return temp;
+        }
+        if(type1 === type2 && typeof index1 === "number") {
+            let temp = [];
+            var pair: [number, string]
+            for (let i = 0; i < TOTAL_TYPES; i++) {
+                pair = [TypeChart[i][index1]*0.5, TypeList[i]];
+                temp.push(pair);
+                console.log(pair);
+            }
+            console.log(temp);
+            return temp;
+        }
+        else if (type1 !== type2 && typeof index1 === "number" && typeof index2 === "number"){
+            let temp = [];
+            var pair: [number, string]
+            for (let i = 0; i < TOTAL_TYPES; i++) {
+                pair = [TypeChart[i][index1]*TypeChart[i][index2]*0.25, TypeList[i]]
+                temp.push(pair);
+                console.log(pair);
+            }
+            console.log(temp);
+            return temp;
+        }
+        else {
+            //alert("La cagaste o wbn");
+            return undefined;
+        }
+    }
+
     useEffect(() => {
         if(typeof getIndex(type1) === "number") {
             setIndex1(getIndex(type1));
@@ -29,20 +87,15 @@ const Calc: React.FC<CalculatorProps> = ({ type1, type2 }) =>{
         if(typeof getIndex(type2) === "number") {
             setIndex2(getIndex(type2));
         }
-        if (typeof index1 === "number") {
-            setResult1(TypeChart[index1]);
-        }
-        if (typeof index2 === "number") {
-            setResult2(TypeChart[index2]);
-        }
-    }, [setIndex1, setIndex2, setResult1, setResult2, index1, index2, type1, type2]);
+        setFinalResult(computeTypeCoverage(type1, type2, index1, index2));
+    }, [setIndex1, setIndex2, index1, index2, type1, type2]);
 
     return(
     <div>
         <h1>{'Type 1: ' + type1}</h1>
         <h1>{'Type 2: ' + type2}</h1>
-        <p>{'Results 1: ' + result1}</p>
-        <p>{'Results 2: ' + result2}</p>
+        <h1>FINAL RESULT IS:</h1>
+        <h3>{finalResult}</h3>
     </div>
     )
   };
