4f8e35edcf620e82a467ddeb84254faa05d0d86e
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-12-30 14:09:38 -0500

    ⚡Updated Pokemon Search
    Updated pokemon image search on teambuilder.

--- src/Components/TeamBuilder/Pokemon.tsx
+++ src/Components/TeamBuilder/Pokemon.tsx
@@ -1,5 +1,4 @@
 import React , {useEffect} from 'react';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -13,26 +12,43 @@ const Pokemon: React.FC<Props> = ({}) =>{
     const [pkmnName, setpkmnName] = React.useState<string>('luxray');
     const [pkmnImg, setpkmnImg] = React.useState(<img className="poke-builder" src={`https://pokeres.bastionbot.org/images/pokemon/405.png`} alt={'luxray'}/>);
     
+    const searchPkmn = async (name: string) => {
+        try {
+            setpkmnImg(<div>
+                Loading...
+            </div>);
+            var apiUrl = 'https://pokeapi.co/api/v2/pokemon/' + name + '/';
+            const resp = await axios.get(apiUrl);
+            setpkmnImg(<img className="poke-builder" src={`https://pokeres.bastionbot.org/images/pokemon/${resp.data.id}.png`} alt={resp.data.name}/>);
+        }
+        catch(err) {
+            setpkmnImg(<div>
+                <img className="poke-builder" src={Bidoof404} alt={'404'}/>
+            </div>);
+            // console.error(err);
+        }
+    }
+
     const handleChangeName = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        let formatName = '';
         let value = '';
         value = e.target.value.toString().toLowerCase();
-        if(value.length > 0) {
-            formatName = value[0].toUpperCase() + value.slice(1, value.length);
-        }
         setpkmnName(value);
     }
 
     const handleKeypress = (e: React.KeyboardEvent<HTMLInputElement>) => {
         var key  = e.key || e.keyCode;
-        if (key === 'Enter' || key === 13) {
-            // searchByName();
+        if (key === 'Enter' || key === 13 || key === 'Tab' || key === 9) {
+            searchPkmn(pkmnName);
         }
+    };
+
+    const handleOut = () => {
+        searchPkmn(pkmnName);
     };
 
     return (
         <Col xs={6} sm={4} md={3} lg={2} className="search-text">
-            <input className="search-teambuilder" value={pkmnName} onChange={handleChangeName} onKeyPress={handleKeypress}>
+            <input className="search-teambuilder" value={pkmnName} onChange={handleChangeName} onKeyPress={handleKeypress} onBlur={handleOut}>
             </input>
             <div className="builder-textdiv">
                 <h1 className="builder-text centered-text">{pkmnName}</h1>
