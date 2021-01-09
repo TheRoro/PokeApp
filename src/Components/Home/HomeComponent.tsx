a4c69bba7fa671bec392add874349278928f2f11
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-01-08 22:00:21 -0500

    🎨 Re-Styled Home Component
    Re-Styled Home Component using styled-components.

--- src/Components/Home/HomeComponent.tsx
+++ src/Components/Home/HomeComponent.tsx
@@ -1,46 +1,50 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Quotes from './Quotes';
-import './Home.css';
-import {Link} from 'react-router-dom';
+import {
+  Title,
+  Text,
+  HomeContainer,
+  FooterContainer,
+  FooterLink,
+  Row,
+  Col,
+  Container
+} from './HomeStyles';
 
 const Home: React.FC<{}> = () => {
     var max = Quotes.length;
     var rand =  Math.floor(Math.random() * Math.floor(max));
     return (
       <>
-      <div className="home">
-          <Container className="full-height">
-            <Row className="align-items-center full-height">
+      <HomeContainer>
+          <Container>
+            <Row className="align-items-center">
               <Col>
                 <Row className="justify-content-center">
                   <Col xs={12}>
-                      <h1 className="homeTitle centered-text">PokeApp</h1>
+                      <Title>PokeApp</Title>
                   </Col>
                   <Col xs={12}>
-                      <p className="homeText centered-text">{Quotes[rand]}</p>
-                      {/* {rand} */}
+                      <Text>{Quotes[rand]}</Text>
                   </Col>
                 </Row>
               </Col>
             </Row>
           </Container>
-      </div>
-      <div className="older">
+      </HomeContainer>
+      <FooterContainer>
         <div className="full-height container-fluid">
-          <Row className="align-items-center full-height">
+          <Row className="align-items-center">
             <Col>
               <Row className="justify-content-center">
                 <Col xs={12}>
-                    <Link to="/older-versions" className="olderText">Older Versions</Link>
+                    <FooterLink to="/older-versions">Older Versions</FooterLink>
                 </Col>
               </Row>
             </Col>
           </Row>
         </div>
-      </div>
+      </FooterContainer>
     </>
     );
 }
---
+++ src/Components/Home/HomeStyles.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {
+    Row as BRow,
+    Col as BCol,
+    Container as BContainer
+} from 'react-bootstrap';
+
+export const HomeContainer = styled.div`
+    height: 75%;
+    width: 100%;
+`
+
+export const FooterContainer = styled.div`
+    height: 15%;
+    width: 100%;
+`
+
+export const Title = styled.h1`
+    font-size:calc(28px + 4vw);
+    font-weight: 900;
+    color: #e6e6e6;
+    text-align: center;
+`
+
+export const Text = styled.p`
+    font-size:calc(12px + 1.3vw);
+    font-weight: 200;
+    color: #88868B;
+    text-align: center;
+`
+
+export const FooterLink = styled(Link)`
+    font-size:calc(10px + 0.6vw);
+    font-weight: 200;
+    color: #b8b8b8;
+    &:hover{
+        text-decoration: none;
+        color: #F4D849;
+    }
+`
+
+export const Row = styled(BRow)`
+    height:100%;
+`
+
+export const Col = styled(BCol)`
+
+`
+
+export const Container = styled(BContainer)`
+    height:100%;
+`
