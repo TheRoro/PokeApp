d6f841b915e1b2db168b4f5a85bc8788333480d6
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-10-01 10:34:57 -0500

    🔗 Update links
    Update links from the older versions of PokeApp

--- src/Components/Navbar/Navbar/NavbarElements.ts
+++ src/Components/Navbar/Navbar/NavbarElements.ts
@@ -1,136 +1,129 @@
-import styled, {css} from 'styled-components';
-import {NavLink as LinkR} from 'react-router-dom';
+import styled from 'styled-components'
+import { NavLink as LinkR } from 'react-router-dom'
 
 export const Nav = styled.nav`
-    height: 10%;
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    font-size: 1rem;
-    background: #121212;
-    /* position: sticky; */
-    top: 0;
-    z-index: 10;
+	height: 10%;
+	display: flex;
+	justify-content: end;
+	align-items: center;
+	font-size: 1rem;
+	background: #121212;
+	/* position: sticky; */
+	top: 0;
+	z-index: 10;
 
-    @media screen and (max-width: 960px) {
-        transition: 0.8s all ease;
-    }
+	@media screen and (max-width: 960px) {
+		transition: 0.8s all ease;
+	}
 `
 
 export const NavbarContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 80px;
-    z-index: 1;
-    width: 100%;
-    padding: 0 24px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	height: 80px;
+	z-index: 1;
+	width: 100%;
+	padding: 0 24px;
 `
 
-const glowColor = '#FFF';
-
-const glow = css`
-    color: #fafafa;
-    text-shadow: 0 0 1px ${glowColor}, 0 0 1px ${glowColor}, 0 0 1px ${glowColor}, 0 0 1px ${glowColor}, 0 0 1px ${glowColor}, 0 0 1px ${glowColor}, 0 0 4px ${glowColor};
-`;
-
 export const NavLogo = styled(LinkR)`
-    color: #fafafa;
-    opacity: .6;
-    justify-self: flex-start;
-    cursor: pointer;
-    font-size: 1.5rem;
-    display: flex;
-    align-items: center;
-    margin-left: 0;
-    font-weight: bold;
-    text-decoration: none;
-    border-bottom: 1px solid #121212;
-    transition: all .2s ease-in-out;
-    &.active {
-        color: #fafafa;
-        opacity: 1;
-        &:hover {
-            opacity: .8;
-        }
-    }
+	color: #fafafa;
+	opacity: 0.6;
+	justify-self: flex-start;
+	cursor: pointer;
+	font-size: 1.5rem;
+	display: flex;
+	align-items: center;
+	margin-left: 0;
+	font-weight: bold;
+	text-decoration: none;
+	border-bottom: 1px solid #121212;
+	transition: all 0.2s ease-in-out;
+	&.active {
+		color: #fafafa;
+		opacity: 1;
+		&:hover {
+			opacity: 0.8;
+		}
+	}
 
-    &:hover {
-        color: #fafafa;
-        text-decoration: none;
-        opacity: 1;
-    }
+	&:hover {
+		color: #fafafa;
+		text-decoration: none;
+		opacity: 1;
+	}
 `
 
 export const MobileIcon = styled.div`
-    display: none;
+	display: none;
 
-    @media screen and (max-width: 990px) {
-        display: block;
-        height: 50px;
-        font-size: 1.8rem;
-        cursor: pointer;
-        color: #fafafa;
-    }
+	@media screen and (max-width: 990px) {
+		display: block;
+		height: 50px;
+		font-size: 1.8rem;
+		cursor: pointer;
+		color: #fafafa;
+	}
 `
 
 export const NavMenu = styled.ul`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    justify-self: flex-end;
-    list-style: none;
-    text-align: center;
-    margin: 0;
-    padding: 0;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	justify-self: flex-end;
+	list-style: none;
+	text-align: center;
+	margin: 0;
+	padding: 0;
 
-    @media screen and (max-width: 990px) {
-        display: none;
-    } 
+	@media screen and (max-width: 990px) {
+		display: none;
+	}
 `
 
 export const NavItem = styled.li`
-    /* height: 80px; */
+	/* height: 80px; */
 `
 
 export const NavLinks = styled(LinkR)`
-    border: none;
-    background: none;
-    cursor: pointer;
-    padding: 0;
-    outline: none!important;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding-bottom: .1rem;
-    margin: 0 1.5rem;
-    height: 100%;
-    font-size: 1.2rem;
-    cursor: pointer;
-    border-bottom: 1px solid #121212;
-    color: #fafafa;
-    opacity: .6;
-    transition: all .2s ease-in-out;
-    &.active {
-        opacity: 1;
-        color: #fafafa;
-        &:hover {
-            opacity: .8;
-        }
-    }
+	border: none;
+	background: none;
+	cursor: pointer;
+	padding: 0;
+	outline: none !important;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding-bottom: 0.1rem;
+	margin: 0 1.5rem;
+	height: 100%;
+	font-size: 1.2rem;
+	cursor: pointer;
+	border-bottom: 1px solid #121212;
+	color: #fafafa;
+	opacity: 0.6;
+	transition: all 0.2s ease-in-out;
+	&.active {
+		opacity: 1;
+		color: #fafafa;
+		&:hover {
+			opacity: 0.8;
+		}
+	}
 
-    &:hover {
-        color: #fafafa;
-        text-decoration: none;
-        opacity: 1;
-    }
+	&:hover {
+		color: #fafafa;
+		text-decoration: none;
+		opacity: 1;
+	}
 `
 
 export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
+	display: flex;
+	align-items: center;
 
-    @media screen and (max-width: 990px) {
-        display: none;
-    }
+	@media screen and (max-width: 990px) {
+		display: none;
+	}
 `
--- src/Components/Older-Versions/OlderVersions.tsx
+++ src/Components/Older-Versions/OlderVersions.tsx
@@ -1,71 +1,82 @@
-import React from 'react';
+import React from 'react'
 import Pokeffective from './Images/Pokeffective.jpg'
 import PokeffectiveRetro from './Images/Pokeffective-Retro.jpg'
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import {
-    Title,
-    Text,
-    OlderContainer,
-    Link,
-    Row,
-    Col,
-    Image
-  } from './OlderVersionsStyles';
+	Title,
+	Text,
+	OlderContainer,
+	Link,
+	Row,
+	Col,
+	Image,
+} from './OlderVersionsStyles'
 
-
-import {
-    Switch,
-    Route,
-    useRouteMatch
-} from "react-router-dom";
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
 
 const OlderVersions: React.FC<{}> = () => {
-    let match = useRouteMatch();
+	let match = useRouteMatch()
 
-    return (
-        <OlderContainer>
-            <Switch>
-            <Route path={`${match.path}/`}>
-                <div className="full-height container-fluid">
-                    <Row className="align-items-center">
-                        <Col xs={12}>
-                            <Row className="justify-content-center">
-                                <Col xs="auto">
-                                    <Title>Older Versions</Title>
-                                </Col>
-                            </Row>
-                            <Row className="justify-content-center">
-                                <Col xs="auto">
-                                    <Text>For all the nostalgics</Text>
-                                </Col>
-                            </Row>
-                            <Row className="mt-3">
-                                <Col xs={12} sm={12} md={6} className="mt-2 mb-3 mb-md-0">
-                                    <Row className="justify-content-center">
-                                        <Col xs="auto">
-                                            <Link href="https://8rb.github.io/Pokeffective-Retro/" target="_blank" rel="noreferrer">
-                                                <Image effect="blur" src={PokeffectiveRetro} alt={'Pokeffective-Retro'}/>
-                                            </Link>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col xs={12} sm={12} md={6} className="mt-5 mt-md-2">
-                                    <Row className="justify-content-center">
-                                        <Col xs="auto">
-                                            <Link href="https://8rb.github.io/Pokeffective/" target="_blank" rel="noreferrer">
-                                                <Image effect="blur" src={Pokeffective} alt={'Pokeffective'}/>
-                                            </Link>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </div>
-            </Route>
-            </Switch>
-        </OlderContainer>
-    );
+	return (
+		<OlderContainer>
+			<Switch>
+				<Route path={`${match.path}/`}>
+					<div className='full-height container-fluid'>
+						<Row className='align-items-center'>
+							<Col xs={12}>
+								<Row className='justify-content-center'>
+									<Col xs='auto'>
+										<Title>Older Versions</Title>
+									</Col>
+								</Row>
+								<Row className='justify-content-center'>
+									<Col xs='auto'>
+										<Text>For all the nostalgics</Text>
+									</Col>
+								</Row>
+								<Row className='mt-3'>
+									<Col xs={12} sm={12} md={6} className='mt-2 mb-3 mb-md-0'>
+										<Row className='justify-content-center'>
+											<Col xs='auto'>
+												<Link
+													href='https://TheRoro.github.io/Pokeffective-Retro/'
+													target='_blank'
+													rel='noreferrer'
+												>
+													<Image
+														effect='blur'
+														src={PokeffectiveRetro}
+														alt={'Pokeffective-Retro'}
+													/>
+												</Link>
+											</Col>
+										</Row>
+									</Col>
+									<Col xs={12} sm={12} md={6} className='mt-5 mt-md-2'>
+										<Row className='justify-content-center'>
+											<Col xs='auto'>
+												<Link
+													href='https://TheRoro.github.io/Pokeffective/'
+													target='_blank'
+													rel='noreferrer'
+												>
+													<Image
+														effect='blur'
+														src={Pokeffective}
+														alt={'Pokeffective'}
+													/>
+												</Link>
+											</Col>
+										</Row>
+									</Col>
+								</Row>
+							</Col>
+						</Row>
+					</div>
+				</Route>
+			</Switch>
+		</OlderContainer>
+	)
 }
 
-export default OlderVersions;
+export default OlderVersions
